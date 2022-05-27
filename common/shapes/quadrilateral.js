cbbd581e610763898579e4a72a403193580a1ddd
DavideB45 <dadewww@gmail.com>
2022-05-27 14:01:59 +0200

    texturedQuad

--- common/shapes/quadrilateral.js
+++ common/shapes/quadrilateral.js
@@ -2,12 +2,19 @@ function Quadrilateral(quad,scale) {
 
 	var nv = 4;
 	this.vertices = new Float32Array(nv  * 3);
-	for(var i = 0; i < nv*3;++i)
+	for(var i = 0; i < nv*3;++i){
 		this.vertices[i] = quad[i];
-	
+	}
+
+	this.texCoords = new Float32Array([
+		0.0,0.0,
+		scale,0.0,
+		scale,scale,
+		0.0,scale
+	]);	
+
 	this.triangleIndices = new Uint16Array(2*3);
 
- 
 	this.triangleIndices[0] = 0;
 	this.triangleIndices[1] = 1;
 	this.triangleIndices[2] = 2;
